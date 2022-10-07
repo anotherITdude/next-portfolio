56ba67f3961d23059f35a3a1732d43e2fbf9f72d
anotherITdude <abhi.baidya@gmail.com>
2022-10-07 22:53:43 +0200

    refactor image link v1

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['https://anotheritdude.github.io/'],
+    //domains: ['https://anotheritdude.github.io/'],
     unoptimized: true
   },
 }
